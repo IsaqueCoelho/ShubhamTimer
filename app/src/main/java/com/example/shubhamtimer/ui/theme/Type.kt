dc0329b1b08ac49f830d2e3b5155ae56647d828e
isaque.coelho <isaque.coelho@dextra-sw.com>
2021-09-03 00:45:28 -0300

    task: Adjust font;

--- app/src/main/java/com/example/shubhamtimer/ui/theme/Type.kt
+++ app/src/main/java/com/example/shubhamtimer/ui/theme/Type.kt
@@ -13,6 +13,11 @@ val TypographyLight = Typography(
         fontSize = 34.sp,
         color = Black
     ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        color = Black
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -36,6 +41,11 @@ val TypographyDark = Typography(
     h4 = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 34.sp,
+        color = White
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
         color = White
     ),
     body1 = TextStyle(
