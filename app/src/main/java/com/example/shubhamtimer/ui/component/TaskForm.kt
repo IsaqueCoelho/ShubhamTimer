2a61142bb88173e3d6c4974f232538c75cf3d85e
isaque.coelho <isaque.coelho@dextra-sw.com>
2021-09-04 20:05:03 -0300

    task: Adjust placeholder and export strings resources;

--- app/src/main/java/com/example/shubhamtimer/ui/component/TaskForm.kt
+++ app/src/main/java/com/example/shubhamtimer/ui/component/TaskForm.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.shubhamtimer.R
 import com.example.shubhamtimer.ui.theme.ShubhamTimerTheme
 
 
@@ -26,17 +29,19 @@ fun TaskForm() {
                 shape = RoundedCornerShape(8.dp)
             ),
     ) {
-        var taskNameValue by remember { mutableStateOf("Task Name") }
-        var taskDescriptionValue by remember { mutableStateOf("Task Description") }
+        var taskNameValue by remember { mutableStateOf("") }
+        var taskDescriptionValue by remember { mutableStateOf("") }
 
         TextField(
             value = taskNameValue,
             onValueChange = { taskNameValue = it },
+            placeholder = { Text(text = stringResource(R.string.taskform_name)) },
             modifier = Modifier.fillMaxWidth(),
         )
         TextField(
             value = taskDescriptionValue,
             onValueChange = { taskDescriptionValue = it },
+            placeholder = { Text(text = stringResource(R.string.taskform_description)) },
             modifier = Modifier.fillMaxWidth(),
         )
     }
